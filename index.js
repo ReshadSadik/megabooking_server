ee092ae55f44686914d5811f40122f0cd886e904
Reshad Sadik <reshadsadik@gmail.com>
2021-10-29 17:02:06 +0600

    env file added

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express = require('express');
 const { MongoClient } = require('mongodb');
 const cors = require('cors');
 const ObjectId = require('mongodb').ObjectId;
+require('dotenv').config();
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 const port = process.env.PORT || 5000;
 
-const uri =
-  'mongodb+srv://megaBooking:Ax3sIFNQagU6Wj3P@cluster0.2ffsd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2ffsd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
